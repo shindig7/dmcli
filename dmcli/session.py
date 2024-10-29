ac3dbf0d1998b9fd4e3aafdc16ccd4e08352b8e0
Jonathan Law <jonathan.law@hey.com>
2024-10-29 09:11:06 -0500

    Session class for managing state

---
+++ dmcli/session.py
@@ -0,0 +1,24 @@
+from uuid import uuid4 as uuid
+from pathlib import Path
+import json
+
+from dmcli.character import PC, NPC 
+
+class Session:
+    def __init__(self, name: str):
+        self.name = name
+        self._id = uuid()
+        self.pcs = []
+        self.npcs = []
+        self.monsters = []
+
+    def load_character(self, file_path: Path) -> None:
+        assert file_path.suffix == ".json", "File must be a JSON file"
+        with open(file_path, "r") as f:
+            data = json.load(f)
+
+        match data["type"]:
+            case "NPC":
+                self.npcs.append(NPC.create_from_json(data))
+            case "PC":
+                self.pcs.append(PC.create_from_json(data))
