3908bb2ac51ef6cd9e7059b85cf41638217f6215
Jonathan Law <jonathan.law@hey.com>
2024-11-23 14:44:26 +0000

    Creates Monster class; Combatant no longer inherits from Character, now PC inherits from both Character and Combatant; add language and senses attributes

--- dmcli/character.py
+++ dmcli/character.py
@@ -2,12 +2,19 @@ from pydantic import BaseModel, Field
 from rich.console import Console
 
 from dmcli.render import render_pc
-from dmcli.special_types import AbilityScore, DamageType, DClass, Gender, Race
+from dmcli.special_types import (
+    AbilityScore,
+    DamageType,
+    DClass,
+    Gender,
+    Race,
+    Size,
+)
 from dmcli.utils import halved
 
 
 class Character(BaseModel):
-    name: str
+    name: str | None
     nickname: str
     race: Race
     gender: Gender
@@ -17,12 +24,14 @@ class Character(BaseModel):
         return cls(**data)
 
 
-class Combatant(Character):
+class Combatant(BaseModel):
     max_hp: int
     current_hp: int
     ac: int = Field(ge=0)
     speed: int
+    ability_scores: dict[AbilityScore, int]
     defenses: dict[str, list[DamageType]]
+    size: Size
 
     def take_damage(
         self, dmg_amount: int, dmg_type: DamageType, magical: bool = False
@@ -43,17 +52,45 @@ class Combatant(Character):
         else:
             self.current_hp += heal_amount
 
+    def is_bloodied(self):
+        return self.current_hp <= halved(self.max_hp)
+
+    @classmethod
+    def create_from_json(cls, data):
+        return cls(**data)
+
 
 class NPC(Character):
     pass
 
 
-class PC(Combatant):
+class PC(Character, Combatant):
     dnd_class: DClass
     temp_hp: int
-    ability_scores: dict[AbilityScore, int]
     proficiencies: dict[str, list[str]]
     level: int = Field(ge=1)
+    languages: list[str]  # TODO: Enum
+    senses: list[str]  # TODO: Enum
 
     def render(self, console: Console):
         render_pc(self, console)
+
+
+class Monster(Combatant):
+    name: str | None = None
+    nickname: str
+    challenge_rating: int
+    traits: list[dict[str, str]]
+    actions: list[dict[str, str]]
+    equipment: list[str]
+
+
+if __name__ == "__main__":
+    console = Console()
+    import json
+
+    with open("data/bugbear.json", "r") as f:
+        data = json.load(f)
+
+    monster = Monster.create_from_json(data)
+    console.print(monster.model_dump())
